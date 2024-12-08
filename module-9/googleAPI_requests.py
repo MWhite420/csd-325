b1dd83cedf9d98f4dffdf36ed9e738f7b0c1a133
Mwhite420 <marwhite4@my365.bellevue.edu>
2024-12-08 14:52:33 -0700

    pushing 12/8

--- module-9/googleAPI_requests.py
+++ module-9/googleAPI_requests.py
@@ -1,3 +1,8 @@
+#Mark White
+#CSD325
+#Assignment9.2
+
+
 import requests
 response = requests.get('http://api.open-notify.org/astros')
 print(response.status_code)
--- module-9/pokeAPI_requests.py
+++ module-9/pokeAPI_requests.py
@@ -1,3 +1,7 @@
+#Mark White
+#CSD325
+#Assignment9.2
+
 import requests
 response = requests.get('https://pokeapi.co/api/v2/generation/?offset=2&limit=2')
 print(response.status_code)
