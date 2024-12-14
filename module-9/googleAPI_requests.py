4fb1de84e1c6a24d5bad5d418a265d1c56b162ae
Mwhite420 <marwhite4@my365.bellevue.edu>
2024-12-14 15:40:27 -0700

    edits

--- module-9/googleAPI_requests.py
+++ module-9/googleAPI_requests.py
@@ -2,8 +2,11 @@
 #CSD325
 #Assignment9.2
 
-
 import requests
-response = requests.get('http://api.open-notify.org/astros')
-print(response.status_code)
-print(response.json())
+import json
+response = requests.get("http://api.open-notify.org/astros")
+def jprint(obj):
+    text = json.dumps(obj, sort_keys=True, indent=4)
+    print(text)
+
+jprint(response.json())
